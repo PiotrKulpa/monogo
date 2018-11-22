183bb13654b8e64d941303f26d6c87a98b68ba14
PiotrKulpa <p.kulpa@onet.eu>
2018-11-22 20:45:37 +0100

    feat: add about us and list components

---
+++ src/components/Aboutus.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Aboutus.css';
+
+class Aboutus extends Component {
+  render() {
+    return (
+      <div className="Aboutus">
+        <h1>About Us component</h1>
+      </div>
+    );
+  }
+}
+
+export default Aboutus;
---
+++ src/components/List.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './List.css';
+
+class List extends Component {
+  render() {
+    return (
+      <div className="List">
+        <h1>List component</h1>
+      </div>
+    );
+  }
+}
+
+export default List;
